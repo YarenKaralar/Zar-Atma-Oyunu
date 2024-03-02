cea02dfce6404a2a5bd7da8cb3a3bdf1c6df9881
Yaren Karalar <152900116+YarenKaralar@users.noreply.github.com>
2024-03-02 20:09:21 +0300

    Zar Atma Oyunu
    C++ dilinde bilgisayarın attığı zarı tekrar tekrar deneyerek denk getirme oyunu

---
+++ KOD/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+#include <time.h>
+using namespace std;
+
+int main()
+{  srand(time(0));
+    char z;
+    int bil,kul,dnm=0;
+    cout<<setw(17)<<"ZAR ATMA OYUNU\n";
+    cout<<"Zar atmak icin 'Z' tusuna basın!:"; 
+    cin>>z;
+    
+     while(z!='Z')
+    {cout<<"Z tusuna basmalisiniz!\n";
+      cin>>z;
+    }
+    
+    bil=rand()%6+1;
+    kul=rand()%6+1;
+    
+    if(bil==kul)
+    dnm=1;
+    
+    while(bil!=kul)
+    { cout<<"Attiginiz sayi "<<kul<<", maalesef dogru sayi degil.\n";
+      cout<<"Tekrar denemek icin 'Z' tusuna basin!:";
+      cin>>z; cout<<endl;
+      while(z!='Z')
+     { cout<<"Z tusuna basmalisiniz!\n";
+       cin>>z;
+     }
+    kul=rand()%6+1;
+    dnm++;
+    }
+    
+    cout<<"Tebrikler! Ayni sayiyi tutturdunuz! (zar->"<<bil<<")"<<endl;
+    cout<<dnm+1<<". denemede dogru sayiyi buldunuz!";
+}
